b30ee59e85d2a45c7c4771bbe1cbe88095a6a7e5
wei.liu <Wei.liu2@b-and-qchina.com>
2019-07-18 14:32:39 +0800

    2019-7-19

--- lib/widget/User/Login.dart
+++ lib/widget/User/Login.dart
@@ -30,7 +30,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 23),
+      // padding: EdgeInsets.only(top: 23),
       child: new Stack(
         alignment: Alignment.center,
         children: <Widget>[
